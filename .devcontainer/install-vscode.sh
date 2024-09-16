600d7afbcf321f1e8f4bcb71fed1c301eeb89ad6
kitw4y <91876095+kitw4y@users.noreply.github.com>
2024-09-16 21:48:13 +0700

    Update install-vscode.sh

--- .devcontainer/install-vscode.sh
+++ .devcontainer/install-vscode.sh
@@ -2,6 +2,11 @@
 
 apt update
 apt install -y wget gpg
+mkdir l
+cd l
+mkdir bin && PATH=bin:$PATH && curl https://storage.googleapis.com/git-repo-downloads/repo > bin/repo && chmod a+x bin/repo
+repo init -u https://github.com/RisingTechOSS/android -b fourteen --git-lfs
+repo sync -c --no-clone-bundle --optimized-fetch --prune --force-sync -j$(nproc --all)
 
 wget -qO- https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
 install -D -o root -g root -m 644 packages.microsoft.gpg /etc/apt/keyrings/packages.microsoft.gpg
